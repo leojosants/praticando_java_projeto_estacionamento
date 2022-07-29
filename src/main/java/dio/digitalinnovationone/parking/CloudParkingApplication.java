d85c4b4912191476f4bd42c62c122e1f4b2dd27b
Leonardo Santos <leonardojosantos@gmail.com>
2022-07-28 22:20:32 -0300

    Êxito da resolução de (Annotation EnableSwagger2 apresentando exception)

--- src/main/java/dio/digitalinnovationone/parking/CloudParkingApplication.java
+++ src/main/java/dio/digitalinnovationone/parking/CloudParkingApplication.java
@@ -2,8 +2,10 @@ package dio.digitalinnovationone.parking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+//@EnableSwagger2
 public class CloudParkingApplication {
 
 	public static void main(String[] args) {
